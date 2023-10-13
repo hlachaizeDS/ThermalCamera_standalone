3feb953316c56a0b225ceffd7aae4fc2b35145dc
HenriLACHAIZE <henri.lachaize@dnascript.co>
2023-10-13 18:26:30 +0200

    Added Snapshot functionality

--- GUI.py
+++ GUI.py
@@ -23,18 +23,36 @@ class MainFrame(Frame):
                               width=self.thermalThread.width * self.thermalThread.zoom,
                               image=img)
 
-        self.ImageLabel.pack()
+        self.ImageLabel.grid(row=1,column=1,columnspan=4)
         self.ImageLabel.Image=img
 
 
         self.tempLabel=Label(self,text="",width=40)
-        self.tempLabel.pack(side="bottom",fill="x")
+        self.tempLabel.grid(row=2,column=1,columnspan=4)
         self.tempmeanLabel=Label(self,text="",width=40)
-        self.tempmeanLabel.pack(side="bottom",fill="x")
+        self.tempmeanLabel.grid(row=3,column=1,columnspan=4)
 
         #Binding to get temperatures live
         self.ImageLabel.bind("<Enter>",self.on_enter)
         self.ImageLabel.bind("<Leave>",self.on_leave)
+
+        #-------------Snapshot feature
+        #Snapshot folder
+        Label(self,height=1).grid(row=4)
+        self.snapshotFolder_label=Label(self,text="Snapshot folder")
+        self.snapshotFolder_label.grid(row=5,column=2, sticky='e')
+        self.snapshotFolder_str=StringVar()
+        self.snapshotFolder=Entry(self,textvariable=self.snapshotFolder_str)
+        self.snapshotFolder.grid(row=5,column=3)
+        #Snapshot name
+        self.snapshotName_label = Label(self, text="Snapshot name")
+        self.snapshotName_label.grid(row=6, column=2, sticky='e')
+        self.snapshotName_str = StringVar()
+        self.snapshotName = Entry(self, textvariable=self.snapshotName_str)
+        self.snapshotName.grid(row=6, column=3)
+        #Snapshot Button
+        self.snapshot_button=Button(self,text="Snap",command=lambda:self.thermalThread.snapshot_in_cycle(1,self.snapshotFolder_str.get(),"",self.snapshotName_str.get()))
+        self.snapshot_button.grid(row=5,column=4,rowspan=2,sticky='w')
 
     def on_enter(self, event):
             self.tempLabel.configure(text="Place cursor on image.")
